ba64c862b72911f1334143da3b3255de717e443d
Marco Munizaga <marco@marcopolo.io>
2022-10-17 11:52:58 -0700

    Update examples/fetch-file-from-kubo/src/libp2p.ts
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- examples/fetch-file-from-kubo/src/libp2p.ts
+++ examples/fetch-file-from-kubo/src/libp2p.ts
@@ -1,9 +1,8 @@
-import { WebTransport as WebTransportLibp2p } from 'js-libp2p-webtransport'
+import { webTransport } from '@libp2p-webtransport'
 import { Noise } from '@chainsafe/libp2p-noise'
 import { createLibp2p, Libp2p } from 'libp2p'
 import { createBitswap } from 'ipfs-bitswap'
 import { MemoryBlockstore } from 'blockstore-core/memory'
-import { Yamux } from '@chainsafe/libp2p-yamux'
 
 type Bitswap = ReturnType<typeof createBitswap>
 
@@ -11,10 +10,8 @@ export async function setup (): Promise<{libp2p: Libp2p, bitswap: Bitswap}> {
   const store = new MemoryBlockstore()
 
   const node = await createLibp2p({
-    transports: [new WebTransportLibp2p()],
-    connectionEncryption: [new Noise()],
-    // Muxer needed here to enable the identify service. A bit of a hack...
-    streamMuxers: [new Yamux()]
+    transports: [webTransport()],
+    connectionEncryption: [() => new Noise()]
   })
 
   await node.start()
